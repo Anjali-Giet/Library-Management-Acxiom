9128687cea34a197a6175f2ca39ab45fe5861bb2
Anjali-Giet <140780158+Anjali-Giet@users.noreply.github.com>
2025-01-11 23:54:54 +0530

    Update login.php

--- Library Management System/user/login.php
+++ Library Management System/user/login.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="register.php">Register</a></li>
+                <li><a href="registration.php">Register</a></li>
             </ul>
         </nav>
     </header>
